0b81e5fe19e90760c0569f782403b18363917c43
Rocky Yang <42904145+Cyhne@users.noreply.github.com>
2020-04-05 22:23:34 -0500

    Turned into an embed

--- commands/info.js
+++ commands/info.js
@@ -1,9 +1,43 @@
+const Discord = require ('discord.js');
+
 module.exports = {
 	name: 'info',
 	description: 'Displays info about this server.',
 	guildOnly: true,
 	cooldown: 10,
 	execute(message) {
-		message.channel.send(`Server Name: ${message.guild.name}\nTotal Members: ${message.guild.memberCount}`);
+		const embed = new Discord.MessageEmbed()
+			.setColor('#00ADFF')
+			.setTitle(`${message.guild.name}`)
+			.setThumbnail(message.guild.iconURL)
+			.setDescription('**Server Information:**')
+			.addFields(
+				{
+					name: '__Total Number of Members:__',
+					value: `**${message.guild.memberCount}**` },
+				{
+					name: '__Online Members__',
+					value: `**${message.guild.members.cache.filter(member => member.presence.status === 'online').size}**`,
+					inline: true,
+				},
+				{
+					name: '__Idle Members__',
+					value: `**${message.guild.members.cache.filter(member => member.presence.status === 'idle').size}**`,
+					inline: true,
+				},
+				{
+					name: '__Offline Members__',
+					value: `**${message.guild.members.cache.filter(member => member.presence.status === 'offline').size}**`,
+					inline: true,
+				},
+				{
+					name: '__Number of Bots__',
+					value: `**${message.guild.members.cache.filter(member => member.user.bot).size}**`,
+					inline: true,
+				},
+			)
+			.setTimestamp()
+			.setFooter('G36');
+		message.channel.send(embed);
 	},
 };
